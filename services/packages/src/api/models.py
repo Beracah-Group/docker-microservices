4ca42a72ac4f407e94a3929ffecb6ceaca78abe2
Scott Businge <businge.scott@andela.com>
2018-10-23 20:19:40 +0300

    add packages models

--- services/packages/src/api/models.py
+++ services/packages/src/api/models.py
@@ -3,3 +3,24 @@ from sqlalchemy.sql import func
 
 database = SQLAlchemy()
 
+class Servicepackages(db.Model):
+    
+    __tablename__ = "servicepackages"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Integer nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+            'price': self.price
+            'description': self.description
+        }
