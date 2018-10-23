e49878f2e38266bece373f21e801b1814e577fb6
Scott Businge <businge.scott@andela.com>
2018-10-23 20:22:45 +0300

    add packages manage, start-up and requirements files

---
+++ services/packages/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Checking postgres is up..."
+while ! nc -z packages-database 5432; do
+  sleep 0.1
+done
+echo "postgres running succesfully...."
+
+echo "Starting app server..."
+python manage.py run --host 0.0.0.0 --port 5000
