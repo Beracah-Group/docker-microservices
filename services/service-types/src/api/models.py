0d66dc8e2b140a2ac06412b511dfc8300630d19a
Scott Businge <businge.scott@andela.com>
2018-10-28 16:53:04 +0300

    modify service types models

--- services/service-types/src/api/models.py
+++ services/service-types/src/api/models.py
@@ -9,9 +9,15 @@ class Servicetypes(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     mode = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
 
     def __init__(self, mode):
         self.mode = mode
+        self.description = description
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
     def to_json(self):
         return {
