3a77d73950fb8a445e07a83af12343efef6f71a7
Scott Businge <businge.scott@andela.com>
2018-11-17 01:28:57 +0300

    delete old implemented services to replace them with two functional ones

--- services/packages/src/db/create.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE production_package_db;
-CREATE DATABASE develop_package_db;
-CREATE DATABASE testing_package_db;
--- services/service-types/src/db/create.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE production_stypes_db;
-CREATE DATABASE develop_stypes_db;
-CREATE DATABASE testing_stypes_db;
